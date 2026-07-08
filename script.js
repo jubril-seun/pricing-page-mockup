adf3115cb357553c3df0c5f93df697a5aafa19c1
“Seun <225109273+jubril-seun@users.noreply.github.com>
2026-07-08 10:24:43 -0400

    Finish html and css styling. Implement javascript for price and dark mode change.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const toggle = document.querySelector(".toggle-checkbox");
+const toggle = document.querySelector("#toggle");
 const price = document.querySelector(".pro-price");
+const drkMode = document.getElementById("toggle-2");
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("change", () => {
     if(toggle.checked){
-        price.textContent = "24";
+        price.textContent = "26";
     } else {
         price.textContent = "18";
+    }
+})
+
+drkMode.addEventListener("change", () => {
+    if(drkMode.checked){
+        document.body.classList.add("dark-mode")
+    } else {
+        document.body.classList.remove("dark-mode")
     }
 })
